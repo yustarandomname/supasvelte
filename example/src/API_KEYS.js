cafbbc35ffc98799df39328d7e0c6b43bf419280
Abel de Bruijn <A.deBruijn-3@student.tudelft.nl>
2021-05-22 22:23:11 +0200

    added an example

---
+++ example/src/API_KEYS.js
@@ -0,0 +1,3 @@
+// find the YOUR URL and YOUR KEY in the API section at INTRODUTION
+export const SUPABASE_URL = "INSERT YOUR URL"
+export const SUPABASE_KEY = "INSERT YOUR KEY"
---
+++ example/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import {Table, supasvelte} from "supasvelte"
+	import {SUPABASE_URL, SUPABASE_KEY} from "./API_KEYS"
+
+	// console.log(supasvelte)
+	supasvelte.init({url: SUPABASE_URL, key: SUPABASE_KEY})
+</script>
+<h1>test</h1>
+
+<Table name="Comments" select={"id,comment, up_votes"} where={"up_votes != 0"} let:data let:error>
+	{JSON.stringify(data)}
+	{@debug data}
+
+	<div slot="error">
+		Error: {JSON.stringify(error)}
+	</div>
+	<div slot="loading">LOADING</div>
+</Table>
