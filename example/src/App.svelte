0c32aa50825f3f9db5247084132c684ea5ecb5a5
Abel de Bruijn <A.deBruijn-3@student.tudelft.nl>
2021-06-03 17:26:40 +0200

    New Tab system

--- example/src/App.svelte
+++ example/src/App.svelte
@@ -9,22 +9,12 @@
 	import TableTab from "./TableTab.svelte"
 	import EmailCard from "./EmailCard.svelte"
 	import {sveltesupa} from "sveltesupa"
-	import {Tabs, Form, Input, Container, Button} from "projectc-components"
+	import {Tabs, TabItem, Form, Input, Container, Button} from "projectc-components"
 
 	const SUPABASE_URL = "https://ulvtjjabqrmyytlqkhhv.supabase.co"
 	const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxOTM3ODMyNywiZXhwIjoxOTM0OTU0MzI3fQ.L32epFv3XaJu7Jk7pd45F9AN8U4-80j_Dc1I4LF8ZZ8'
 
 	sveltesupa.init({url: SUPABASE_URL, key: SUPABASE_KEY})
-
-	const tabs = [
-		{component: TableTab, props: {title: "Tabel default"}},
-		{component: TableTab, props: {title: "Select", select: "id, name"}},
-		{component: TableTab, props: {title: "Nested select", select: "*, organisation(name, type(*)) "}},
-		{component: TableTab, props: {title: "Limit", limit: 4}},
-		{component: TableTab, props: {title: "Order", order: ">"}},
-		{component: TableTab, props: {title: "Range", range: [3,4]}},
-		{component: TableTab, props: {title: "Where", where: ["id", ">=", "2"]}}
-	]
 
 	let email;
 	let password;
@@ -50,7 +40,15 @@
 		<Button on:click={() => signOut()}>Sign out</Button>
 	</Container>
 
-	<Tabs {tabs} />
+	<Tabs tabs={["Default", "Select", "Nested select", "Limit", "Order", "Range", "Where"]}>
+		<TabItem><TableTab title="Default" /></TabItem>
+		<TabItem><TableTab title="Select" select="id, name" /></TabItem>
+		<TabItem><TableTab title="Nested select" select="*, organisation(name, type(*)) " /></TabItem>
+		<TabItem><TableTab title="Limit" limit="limit" /></TabItem>
+		<TabItem><TableTab title="Order" order=">" /></TabItem>
+		<TabItem><TableTab title="Range" range={[3,4]} /></TabItem>
+		<TabItem><TableTab title="Where" order={["id", ">=", "2"]} /></TabItem>
+	</Tabs>
 
 	<div slot="logged-out">
 		<Form header="Sign in" submitMessage="sign in" on:submit={() => signIn({email, password})}>
